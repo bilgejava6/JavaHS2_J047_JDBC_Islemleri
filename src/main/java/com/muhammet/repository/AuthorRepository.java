74c03bee0f240880e2e3d8d20ef0813d9155d7e2
bilgejava6 <bilgejava6@gmail.com>
2025-01-12 11:29:52 +0300

    AuthorRepository güncwellendi.

--- src/main/java/com/muhammet/repository/AuthorRepository.java
+++ src/main/java/com/muhammet/repository/AuthorRepository.java
@@ -1,70 +1,40 @@
 package com.muhammet.repository;
-
 import com.muhammet.entity.Author;
+import com.muhammet.utility.DataBaseManagement;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.util.List;
 
 public class AuthorRepository implements Repository<Author>{
+    // Todo: ARAŞTIRILACAK, Geç Bağlanma
+    private final DataBaseManagement dataBaseManagement;
+    private String SQL;
+    public AuthorRepository(){
+        dataBaseManagement = new DataBaseManagement();
+//        String E = "";
+//        String K = "";
+//        String ifade = "spor salonunda ["+E+"] ile ["+K+"] bugün buluşacak";
+    }
 
-    @Override
     public void save(Author author) {
-        try{
-            /**
-             * String ad = "Ali";
-             * String soyad = "TAŞ";
-             * String bitisikIsim = ad+ " " +soyad;
-             */
-            Driver.class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5435/KitapYonetimDB";
-            String user = "postgres";
-            String password = "root";
-            Connection conn = DriverManager.getConnection(url,user,password);
-            String SQL = "INSERT INTO authors(first_name,last_name,birth_date,city)  " +
-                    " VALUES(?,?,?,?)";
-            PreparedStatement ps = conn.prepareCall(SQL);
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setDate(3, author.getBirthDate());
-            ps.setString(4, author.getCity());
-            ps.executeUpdate();
-            conn.close();
-        }catch (Exception exception){
-            System.err.println("Yazar Eklemede hata...: "+ exception.getMessage());
-        }
+        SQL = "INSERT INTO author(first_name, last_name, birth_date, city) VALUES" +
+                "('"+author.getFirstName()+"','"+author.getLastName()+"','"+author.getBirthDate()+"','"+author.getCity()+"')";
+        dataBaseManagement.executeQuery(SQL);
     }
 
-    @Override
     public void update(Author author) {
-        try{
-            Driver.class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5435/KitapYonetimDB";
-            String user = "postgres";
-            String password = "root";
-            Connection conn = DriverManager.getConnection(url,user,password);
-            String SQL = "update authors set first_name=?, last_name=?, birth_date=?, city=? where id=?";
-            PreparedStatement ps = conn.prepareCall(SQL);
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setDate(3, author.getBirthDate());
-            ps.setString(4, author.getCity());
-            ps.setInt(5, author.getId());
-            ps.executeUpdate();
-            conn.close();
-        }catch (Exception exception){
-            System.err.println("Yazar Güncellemede hata...: "+ exception.getMessage());
-        }
+        SQL = "UPDATE author SET first_name='"+author.getFirstName()+"'," +
+                " last_name='"+author.getLastName()+"'," +
+                " birth_date='"+author.getBirthDate()+"'," +
+                " city='"+author.getCity()+"'" +
+                " WHERE id="+author.getId();
+        dataBaseManagement.executeQuery(SQL);
     }
 
-    @Override
     public void delete(Integer id) {
-
+        SQL = "DELETE FROM author WHERE id="+id;
+        dataBaseManagement.executeQuery(SQL);
     }
 
-    @Override
     public List<Author> findAll() {
         return List.of();
     }
