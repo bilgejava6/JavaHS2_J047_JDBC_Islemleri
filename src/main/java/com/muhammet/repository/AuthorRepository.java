c08c1ae5c3b466543b43cbf70e0781c4bd385693
bilgejava6 <bilgejava6@gmail.com>
2025-01-12 12:05:59 +0300

    AuthorRepository listeleme eklendi.

--- src/main/java/com/muhammet/repository/AuthorRepository.java
+++ src/main/java/com/muhammet/repository/AuthorRepository.java
@@ -2,6 +2,9 @@ package com.muhammet.repository;
 import com.muhammet.entity.Author;
 import com.muhammet.utility.DataBaseManagement;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AuthorRepository implements Repository<Author>{
@@ -36,6 +39,30 @@ public class AuthorRepository implements Repository<Author>{
     }
 
     public List<Author> findAll() {
-        return List.of();
+        SQL = "SELECT * FROM author";
+        ResultSet rs = dataBaseManagement.getResultSet(SQL);
+        /**
+         * ResultSet -> DB den sorgu neticesinde dönen tablonun içerisinde bulunduğu java nesnesi
+         * bunun içerisinde tablo satır satır bulunmaktadır. Bu veriyi okuyarak bir listeye aktar
+         * mamız gereklidir.
+         */
+        List<Author> authorList = new ArrayList<>();
+        try{
+            while (rs.next()){ // resultset içindeki tüm satırları tek tek dönmek için kullanıyoruz.
+                // resultset içerisinde konumlandığım satırdaki verileri okuyorum.
+                int id = rs.getInt("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                Date birthDate = rs.getDate("birth_date");
+                String city = rs.getString("city");
+                // okunan veriler ile author nesnesi oluşturuyoruz.
+                Author author = new Author(id,firstName,lastName,birthDate,city,null,null,null);
+                // oluşturduğum nesneyi listenin içerisine ekliyorum.
+                authorList.add(author);
+            }
+        }catch (Exception exception){
+            System.out.println("okuma işleminde hata...: "+ exception.getMessage());
+        }
+        return authorList;
     }
 }
